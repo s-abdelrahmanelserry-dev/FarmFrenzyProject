15805096ce52e80a53eaf2c72c4b40a54367b948
s-abdelrahmanelserry-dev <s-abdelrahman.elserry@zewailcity.edu.eg>
2026-05-11 17:51:16 +0300

    Update StatusBar.h

--- UI/StatusBar.h
+++ UI/StatusBar.h
@@ -1,1 +1,8 @@
+#pragma once
+#include "../CMUgraphicsLib/CMUgraphics.h"
 
+class StatusBar {
+public:
+    // Pass the window and the game data here
+    void Draw(window* pWind, int timer, int goal, int level, int animals);
+};
