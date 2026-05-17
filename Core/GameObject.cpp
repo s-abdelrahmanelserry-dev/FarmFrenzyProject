91030a95592f978ac90876f9b14f483de3cde808
s-abdelrahmanelserry-dev <s-abdelrahman.elserry@zewailcity.edu.eg>
2026-05-17 14:23:04 +0300

    Add collision detection method to GameObject
    Implemented AABB collision detection in GameObject.

--- Core/GameObject.cpp
+++ Core/GameObject.cpp
@@ -1,20 +1,42 @@
 #include "GameObject.h"
 
-GameObject::GameObject(Game* r_pGame, point ref, int r_width, int r_height, color fc, color bc) : Drawable (r_pGame, ref, r_width, r_height)
+GameObject::GameObject(Game* r_pGame, point ref, int r_width, int r_height, color fc, color bc) 
+    : Drawable(r_pGame, ref, r_width, r_height)
 {
-	fillColor = fc;
-	borderColor = bc;
+    fillColor = fc;
+    borderColor = bc;
 }
 
 void GameObject::setRefPoint(point p)
 {
-	RefPoint = p;
+    RefPoint = p;
 }
 
-
-// you should implement this function knowing that each game object has refPoint, width and height 
-// for simplicity, you can consider all game objects are rectangles
+// Implemented using Axis-Aligned Bounding Box (AABB) collision detection
 bool GameObject::CollisionDetection(const GameObject& gObj)
 {
-	return false;
+    // Object 1 (this) bounds
+    int left1 = this->RefPoint.x;
+    int right1 = this->RefPoint.x + this->width;
+    int top1 = this->RefPoint.y;
+    int bottom1 = this->RefPoint.y + this->height;
+
+    // Object 2 (gObj) bounds
+    int left2 = gObj.RefPoint.x;
+    int right2 = gObj.RefPoint.x + gObj.width;
+    int top2 = gObj.RefPoint.y;
+    int bottom2 = gObj.RefPoint.y + gObj.height;
+
+    // Check if the rectangles overlap on both the X and Y axes
+    // If any of these conditions are true, there is NO collision.
+    if (right1 < left2 ||   // 'this' is completely to the left of 'gObj'
+        left1 > right2 ||   // 'this' is completely to the right of 'gObj'
+        bottom1 < top2 ||   // 'this' is completely above 'gObj'
+        top1 > bottom2)     // 'this' is completely below 'gObj'
+    {
+        return false; 
+    }
+
+    // If none of the above are true, they must be overlapping
+    return true;
 }
